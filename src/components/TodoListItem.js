333fa6b90f11f5f735f2c504d665ab2ca6220162
연승은 <seungeun_yeon@tmax.co.kr>
2020-11-01 00:36:08 +0900

    refactor: react-virtualized를 사용한 렌더링 최적화 적용

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,19 +7,27 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodolListItem = ({ todo, onRemove, onToggle }) => {
+const TodolListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
 };
 
-export default React.memo(TodolListItem);
+export default React.memo(
+  TodolListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
